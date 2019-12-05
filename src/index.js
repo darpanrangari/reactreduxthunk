fbe11908db00658482a8de83300847904eb8ee21
darpan.rangari <darpan.rangari@lloydsbanking.com>
2019-12-05 19:27:58 +0000

    react-redux-thunk boilerplate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import GithubData from './containers/GithubData';
+import rootReducer from './reducers';
+import { fetchGithubData } from './actions/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.dispatch(fetchGithubData());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <GithubData />
+    </Provider>, document.getElementById('root'));
